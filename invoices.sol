cd9a0aff94bba838df825e7e740f6e5064e85249
Theodore Brinkofski <theodorebrinkofski@Theodores-MBP-2.fios-router.home>
2022-10-30 17:51:12 -0400

    first draft to be deployed

--- invoices.sol
+++ invoices.sol
@@ -2,31 +2,32 @@
 pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Invoices {
 
     uint256 public invoiceCount;
     uint256 public fee; //2.5 == 250
     
-    //admin
     address public admin;
-    address public priceFeed;
+    address public oracle;
 
     struct Invoice {
         string metadata;
-        bool status;
+        bool open;
         uint256 price;
         uint256 oraclePrice;
         uint256 created;
+        uint256 closed;
         uint256 paid;
         address creator;
     }
 
-    mapping(uint256 => Invoice) public invoices; // invoice id to invoice data
+    mapping(uint256 => Invoice) public invoices; // invoiceId => Invoice
 
-    constructor(address _priceFeed) {
+    constructor(address _oracle) {
         fee = 100;
-        priceFeed = _priceFeed;
+        oracle = _oracle;
     }
 
     event InvoiceCreated(
@@ -51,7 +52,7 @@ contract Invoices {
 
     function create(
         string memory _metadata,
-        uint256 _price
+        uint256 _price //in USD
     ) public returns (uint256) {
 
         invoiceCount++;
@@ -61,7 +62,7 @@ contract Invoices {
         _invoice.metadata = _metadata;
         _invoice.price = _price;
         _invoice.creator = msg.sender;
-        _invoice.status = true;
+        _invoice.open = true;
         _invoice.created = block.number;
 
         invoice[invoiceCount] = _invoice;
@@ -80,7 +81,7 @@ contract Invoices {
         payable
     {
         require(
-            invoices[_invoiceId].status == true,
+            invoices[_invoiceId].open == true,
             "Invoice not open."
         );
 
@@ -93,8 +94,9 @@ contract Invoices {
 
         require(msg.value >= invoiceTotal, "Amount paid is less than total");
 
-        _invoice.paid = block.number;
-        _invoice.status = false;
+        _invoice.paid = true;
+        _invoice.open = false;
+        _invoice.closed = block.number;
         
 
         if (msg.value > invoiceTotal) {
@@ -102,95 +104,63 @@ contract Invoices {
             (bool sent,) = msg.sender.call{
                 value: (msg.value - invoiceTotal)
             }("");
-            require(sent, "Failed to Send Ether");
+            require(sent, "Failed to Send Extra Ether back to Payer");
         }
+
+        //pay creator
+        (bool sent,) = _invoice.creator.call{
+            value: (invoiceTotal)
+            }("");
+        require(sent, "Failed to Send Ether to Invoice Creator");
+
+        //pay fee
+        (bool sent,) = owner.call{
+            value: (invoiceFee)
+            }("");
+        require(sent, "Failed to Send Ether fee to Contract Owner");
+
 
         emit InvoicePaid(
             _invoiceId
         );
 
-        //add forwarding payment to creator
-        //better way to know if invoice is paid other than status being true or false
 
     }
 
-
-    //DAO controls
-    function updateRules(uint256 _variable, uint256 _value)
-        public
-        returns (bool)
-    {
-        require(dao == msg.sender);
-        if (_variable == 0) {
-            //edit fee
-            marketplaceFee = _value;
-        } 
-
+    //edit Settings
+    //update fee
+    function updateFee(uint256 _fee) public onlyOwner returns (bool) {
+        fee = _fee
         return true;
     }
 
-    //Admin controls
-    function updateAddress(uint256 _value, address _newAddress) public {
-        require(msg.sender == admin, "Only company can change address");
-        if (_value == 0) {
-            //DAO Address
-            dao = _newAddress;
-        } else if (_value == 1) {
-            //Token Address
-            admin = _newAddress;
-        }else if (_value == 2) {
-            //Token Address
-            m4sToken = _newAddress;
-        } else if (_value == 3) {
-            //Price Feed Address
-            priceFeed = _newAddress;
-        }
+    //update oracle address
+    function updateOracle(uint256 _oracle)
+        public
+        onlyOwner
+        returns (bool)
+    {
+        oracle = _oracle;
+        return true;
     }
 
-
-
-    //edit Item
-    function editPrice(uint256 _itemId, uint256 _value) public {
-        require(
-            msg.sender == itemInfo[_itemId].owner,
-            "Only item owner can edit"
-        );
-
-        itemInfo[_itemId].price = _value;
-
-        emit ItemUpdated(_itemId);
+    //edit Invoice
+    function editPrice(uint256 _itemId, uint256 _price) public onlyCreator {
+        invoices[_invoiceId].price = _price;
+        emit InvoiceUpdated(_invoiceId);
     }
 
-    function editMetadata(uint256 _itemId, string memory _metadata) public {
-        require(
-            msg.sender == itemInfo[_itemId].owner,
-            "Only item owner can edit"
-        );
-
-        itemInfo[_itemId].metadata = _metadata;
-
-        emit ItemUpdated(_itemId);
+    function editMetadata(uint256 _invoiceId, string memory _metadata) public onlyCreator {
+        invoices[_invoiceId].metadata = _metadata;
+        emit InvoiceUpdated(_invoiceId);
     }
 
-    function editState(uint256 _itemId, bool _isLive) public {
-        require(
-            msg.sender == itemInfo[_itemId].owner,
-            "Only item owner can edit"
-        );
-
-        itemInfo[_itemId].isLive = _isLive;
-
-        emit ItemUpdated(_itemId);
+    function editState(uint256 _invoiceId, bool _open) public onlyCreator {
+        invoices[_invoiceId].open = _open;
+        emit InvoiceUpdated(_invoiceId);
     }
 
-    //internal
-    function _transferEarnings(uint256 _amount) internal {
-        (bool sent, bytes memory data) = dao.call{value: _amount}("");
-        data;
-        require(sent, "Failed to Send Ether");
-    }
-
-    //get Chain Link Price
+    //get Native Asset Price from Chain Link Oracle
     function getLatestPrice() public view returns (int256) {
         (
             ,
@@ -199,7 +169,7 @@ contract Invoices {
             ,
             ,
 
-        ) = AggregatorV3Interface(priceFeed).latestRoundData();
+        ) = AggregatorV3Interface(oracle).latestRoundData();
         // avax main net 0x0A77230d17318075983913bC2145DB16C7366156
         //avax test net 0x5498BB86BC934c8D34FDA08E81D444153d0D06aD
         return price;
